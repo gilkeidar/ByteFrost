ab4cb87040af7ab85d2b4e96395dcd5602a1e195
Gil Keidar <gilkeidar2019@gmail.com>
2022-06-21 15:04:08 -0700

    Added Program README and cleaned memdump

---
+++ Programs/README.md
@@ -0,0 +1,9 @@
+# Programs
+
+Here is a list of included programs.
+
+> Note: ByteFrost assembly programs have the file extension `.asm` and can be compiled to `.a` (this rewrites the assembly program into microcode in ASCII, useful for uploading a program to the program EEPROM using Arduino code) and `.bin` (which compiles the assembly directly to binary)
+
+## Assembly Programs
+
+* `memdump.asm` - (fill program information here) - written by Ron Keidar
--- Programs/memdump.asm
+++ Programs/memdump.asm
@@ -1,6 +1,6 @@
 LDR R0, #0x20   // how many bytes to dump
 LDR R1, #0      // Start address
-LDR R2, #0x10 // New line
+LDR R2, #0x10   // New line
 OUT R2, A
 LDR R2, #0x3a   // ASCII ':'    
 OUT R1, I       // Print address
@@ -15,6 +15,6 @@ BNE #16         // If there are still bytes to print
 BRK
 NOP 
 LDR R2 #0x07   
-AND R2, R2, R1   // If R1 is multiple of 8
-BNE #10           // If address is not multiply, keep printing 
-JMP #3           // Start a new line
+AND R2, R2, R1  // If R1 is multiple of 8
+BNE #10         // If address is not multiply, keep printing 
+JMP #3          // Start a new line
