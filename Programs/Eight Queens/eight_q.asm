2e17203f1c1dad78438e941a5708bc052da51a30
Ron-Keidar <ronkeidar45@gmail.com>
2022-06-25 14:02:25 -0700

    Add debug printout

--- Programs/Eight Queens/eight_q.asm
+++ Programs/Eight Queens/eight_q.asm
@@ -1,58 +1,61 @@
 // Setup
 LDR R0, #0	// row (R0) = 0
 SMR R0, R0	// board[0] = 0
-// for_loop_start
+// :for_loop_start
 MOV R1, R0	// i (R1) = row
 LMR R2, R0	// col (R2) = board[row]
 DEC R1      // i = row - 1
-// for_loop_check
-BMI #20 	// if i >= 0, continue (branch to valid if i < 0)
+// :for_loop_check
+BMI #20 	// if i >= 0, continue (branch to :valid if i < 0)
 // for_loop_body
 // (Horizontal check)
 LMR R3, R1	// R3 = board[i]
 SUB R3, R3, R2	// R3 = board[i] - col
-BEQ #25		// if board[i] == col, branch to not_valid
+BEQ #24		// if board[i] == col, branch to :not_valid
 // Diagonal check 1
 SUB R3, R3, R0	// R3 = board[i] - col - row
 ADD R3, R3, R1	// R3 = board[i] - col - row + i
-BEQ #25		// if board[i] == col + (row - i), branch to not_valid
+BEQ #24		// if board[i] == col + (row - i), branch to :not_valid
 // Diagonal check 2
 LMR R3, R1	// R3 = board[i]
 SUB R3, R3, R2	// R3 = board[i] - col
 ADD R3, R3, R0	// R3 = board[i] - col + row
 SUB R3, R3, R1	// R3 = board[i] - col + row - i
-BEQ #25		// if board[i] == col - (row - i), branch to not_valid
+BEQ #24		// if board[i] == col - (row - i), branch to :not_valid
 // for_loop_update
 DEC R2              // i--
-JMP #6 // goto for_loop_check
-// valid
-OUT R0, A   // Print row
+JMP #6 // goto :for_loop_check
+// :valid
 INC R0		// row++
 LDR R2, #0	// R2 = 0
 SMR R2, R0	// board[row] = 0
-JMP #42		// Goto while_loop_check
+JMP #41		// Goto :while_loop_check
 // not_valid
 LMR R2, R0	// R2 = board[row]
 INC R2		// R2 = board[row] + 1
 SMR R2, R0	// board[row] = board[row] + 1
 LDR R3, #8	// R3 = 8
 SUB R3, R2, R3	// R3 = R2 - 8
-BMI #42		// if board[row] < 8, go to while_loop_check
+BMI #41		// if board[row] < 8, go to :while_loop_check
 DEC R0		// row--
 LMR R2, R0	// R2 = board[row]
 INC R2		// R2 = board[row] + 1
 SMR R2, R0	// board[row] = board[row] + 1
 LDR R3, #8	// R3 = 8
 SUB R3, R2, R3	// R3 = R2 - 8
-BMI #42		// if board[row] < 8, go to while_loop_check
+BMI #41		// if board[row] < 8, go to :while_loop_check
 DEC R0		// row--
 LMR R2, R0	// R2 = board[row]
 INC R2		// R2 = board[row] + 1
 SMR R2, R0	// board[row] = board[row] + 1
-// while_loop_check
+// :while_loop_check
+OUT R0, A   // Print row
+LMR R2, R0	// R2 = board[row]
+OUT R2, A
+OUT #0x20, A
 LDR R3, #8	// R3 = 8
 SUB R3, R0, R3	// R3 = row - 8
-BMI #3		// if row < 8, branch to for_loop_start
+BMI #3		// if row < 8, branch to :for_loop_start
 // Print Solution (setup)
 LDR R2, #0	// R2 = 0
 LDR R1, #65	// R1 = 'A'
@@ -64,5 +67,5 @@ INC R2		// R2++
 INC R1		// R1++
 LDR R3, #8	// R3 = 8
 SUB R3, R2, R3	// R3 = R2 - 8
-BMI #47		// if R2 < 8, go to print_solution
+BMI -7		// if R2 < 8, go to print_solution
 BRK
