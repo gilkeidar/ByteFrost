fba8e4d4c2df923354053452c40a56b8f54b2a8e
Ron Keidar <66293354+ron-keidar@users.noreply.github.com>
2025-11-28 21:50:42 -0800

    Update ldw_sdw_test.asm
    Fixed LDW and SDW bug (using ROM pages by mistake; changed to use RAM pages)
    
    Co-Authored-By: Gil Keidar <57346626+gilkeidar@users.noreply.github.com>

--- Programs/LDW_SDW_Test/ldw_sdw_test.asm
+++ Programs/LDW_SDW_Test/ldw_sdw_test.asm
@@ -41,24 +41,24 @@
 //		*	PC's high byte is either 0x00 or 0x01 (likely 0x00 since this
 //			program should be fairly short).
 //		*	BP's high byte cannot be set using LSP.
-//		*	Set DP's high byte to 0x02 and SP's high byte to 0x04 (to avoid
+//		*	Set DP's high byte to 0x30 and SP's high byte to 0x32 (to avoid
 //			clashing).
 //			*	SP's low byte starts at 0x00 (due to boot-time reset) but DP's
 //				low byte contains garbage initially.
 //			*	Hence, after this, without changing SP or DP, their ranges for
 //				LDW and SDW are:
-//				1.	SP: 0x0400 -> [0x03f0, 0x04ef]
-//				2.	DP: 0x02?? -> [0x01f0, 0x03ee] (given lowest and highest
+//				1.	SP: 0x3200 -> [0x31f0, 0x32ef]
+//				2.	DP: 0x30?? -> [0x2ff0, 0x30fe] (given lowest and highest
 //					values of DP's low byte, respectively).	
 //	3.	SP Test
 //		1.	Load a value to R0.
-//		2.	Push R0 to the stack (now SP is at 0x0401 due to the current stack
+//		2.	Push R0 to the stack (now SP is at 0x3201 due to the current stack
 //			implementation being empty-ascending).
-//		3.	Use LDW to read the value at SP - 1 (0x0400 where R0's value was
+//		3.	Use LDW to read the value at SP - 1 (0x3200 where R0's value was
 //			just pushed).
-//		4.	Use SDW to change the value at 0x0400 to something else.
-//		5.	Pop the value from the stack to R0 (will pop the value at 0x0400).
-//		6.	Verify that R0's value is the value that SDW wrote at 0x0400.
+//		4.	Use SDW to change the value at 0x3200 to something else.
+//		5.	Pop the value from the stack to R0 (will pop the value at 0x3200).
+//		6.	Verify that R0's value is the value that SDW wrote at 0x3200.
 //	4.	DP Test
 //		1.	Use SDW to write a value at DP and verify it is correct using LDW.
 //		2.	Use SDW to write a value at DP + 127 and verify it is correct using
@@ -68,9 +68,6 @@
 //	5.	PC Test
 //		1.	Load byte from a particular instruction (e.g., the immediate byte)
 //			using LDW, PC, #X and ensure it is the expected byte.
-//		2.	Store byte to a particular instruction (e.g., change the immediate
-//			byte using SDW, PC, #X and then read it using LDW to ensure it is
-//			the correct value.
 //
 //	1.	Print test name ("LDW AND SDW TEST\n") (instructions 0 - )
 OUT _L, A
@@ -92,18 +89,18 @@ OUT _T, A
 OUT NEW_LINE, A
 
 //	2.	Set high bytes of DP and SP using LSP.
-LSP %HDP, #0x02
-LSP %HSP, #0x04
+LSP %HDP, #0x30
+LSP %HSP, #0x32
 
 //	3.	SP Test
 //		1.	Load a value to R0.
 LDR R0, #0x28
 
-//		2.	Push R0 to the stack (now SP is at 0x0401 due to the current stack
+//		2.	Push R0 to the stack (now SP is at 0x3201 due to the current stack
 //			implementation being empty-ascending).
 PUSH R0
 
-//		3.	Use LDW to read the value at SP - 1 (0x0400 where R0's value was
+//		3.	Use LDW to read the value at SP - 1 (0x3200 where R0's value was
 //			just pushed).
 LDW R1, %SP, #-1
 
@@ -121,14 +118,14 @@ OUT SPACE, A
 OUT R1, I
 OUT NEW_LINE, A
 
-//		4.	Use SDW to change the value at 0x0400 to something else.
+//		4.	Use SDW to change the value at 0x3200 to something else.
 LDR R2, #0x55
 SDW R2, %SP, #-1
 
-//		5.	Pop the value from the stack to R0 (will pop the value at 0x0400).
+//		5.	Pop the value from the stack to R0 (will pop the value at 0x3200).
 POP R0
 
-//		6.	Verify that R0's value is the value that SDW wrote at 0x0400.
+//		6.	Verify that R0's value is the value that SDW wrote at 0x3200.
 //			printf("R2: %d R0: %d\n", R2, R0);
 OUT _R, A
 OUT #2, A
@@ -209,7 +206,6 @@ OUT NEW_LINE, A
 //			Instruction to use: LDR R0, #0x82 (high byte = #0x82)
 .define	1	LDR_INSTR_OFFSET_1	-33
 .define	1	LDR_INSTR_OFFSET_2	-63
-.define	1	LDR_INSTR_OFFSET_3	-65
 
 LDR R0, #0x82
 LDW R1, %PC, LDR_INSTR_OFFSET_1
@@ -232,22 +228,22 @@ OUT NEW_LINE, A
 //			byte using SDW, PC, #X and then read it using LDW to ensure it is
 //			the correct value.
 
-LDR R0, #0x22
-SDW R0, %PC, LDR_INSTR_OFFSET_2
-LDW R1, %PC, LDR_INSTR_OFFSET_3
+//LDR R0, #0x22
+//SDW R0, %PC, LDR_INSTR_OFFSET_2
+//LDW R1, %PC, LDR_INSTR_OFFSET_3
 
 //			printf("R0: %d R1: %d\n", R0, R1); (instructions: 12)
-OUT _R, A
-OUT #0, A
-OUT COLON, A
-OUT SPACE, A
-OUT R0, I
-OUT SPACE, A
-OUT _R, A
-OUT #1, A
-OUT COLON, A
-OUT SPACE, A
-OUT R1, I
-OUT NEW_LINE, A
+//OUT _R, A
+//OUT #0, A
+//OUT COLON, A
+//OUT SPACE, A
+//OUT R0, I
+//OUT SPACE, A
+//OUT _R, A
+//OUT #1, A
+//OUT COLON, A
+//OUT SPACE, A
+//OUT R1, I
+//OUT NEW_LINE, A
 
 BRK
