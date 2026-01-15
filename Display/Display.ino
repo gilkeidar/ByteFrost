839417c6d9afd9be1640f467dad8aa17e63853b1
Ron Keidar <ronkeidar45@gmail.com>
2026-01-14 18:26:46 -0800

    Clean display ino

--- Display/Display.ino
+++ Display/Display.ino
@@ -75,35 +75,30 @@ typedef struct queue_item {
 // initialize the library by associating any needed LCD interface pin
 // with the arduino pin number it is connected to
 const int rs = 12, en = 11, d0 = 1, d1 = 0, d2 = 13, d3 = 3, d4 = 4, d5 = 5, d6 = 6, d7 = 7; // d0 and d1 are switched in the implementation
-int _data_pins[] = {d0, d1, d2, d3, d4, d5, d6, d7};    // For modified write4bits we're using
+int display_data_out[] = {d0, d1, d2, d3, d4, d5, d6, d7};    // For modified write4bits we're using
 LiquidCrystal lcd(rs, en, d0, d1, d2, d3, d4, d5, d6, d7);
 
-void pulseEnable(void) {
+void pulseEnable(void)
+{
   clrPin(en);
   delayMicroseconds(10);
   setPin(en);
   delayMicroseconds(15);
   clrPin(en);
   delayMicroseconds(12);
-  
-  //digitalWrite(en, LOW);
-  //delayMicroseconds(1);    
-  //digitalWrite(en, HIGH);
-  //delayMicroseconds(1);    // enable pulse must be >450ns
-  //digitalWrite(en, LOW);
-  //delayMicroseconds(1);   // commands need > 37us to settle
 }
 
+#if 0 // not used
 void write4bits(uint8_t value) {
   //for (int i = 0; i < 4; i++) {
-  //  digitalWrite(_data_pins[i], (value >> i) & 0x01);
+  //  digitalWrite(display_data_out[i], (value >> i) & 0x01);
     /*if ((value >> i) & 0x01)
     {
-      setPin(_data_pins[i]);
+      setPin(display_data_out[i]);
     }
     else
     {
-      clrPin(_data_pins[i]);
+      clrPin(display_data_out[i]);
     }*/
   //}
 
@@ -113,6 +108,7 @@ void write4bits(uint8_t value) {
 
   pulseEnable();
 }
+#endif
 
 void write8bits(uint8_t value) {
   //PORTD = value;
@@ -123,33 +119,16 @@ void write8bits(uint8_t value) {
   int i;
   for (i = 0; i < 8; i++)
   {
-    digitalWrite(_data_pins[i], (value >> i) & 0x01);
+    digitalWrite(display_data_out[i], (value >> i) & 0x01);
   }
 
   pulseEnable();
 }
 
 // write either command or data, with automatic 4/8-bit selection
-void send(uint8_t value, uint8_t mode) {
+void send(uint8_t value, uint8_t mode)
+{
   digitalWrite(rs, mode);
-  /*if (mode)
-    setPin(rs);
-  else
-    clrPin(rs);
-
-  delayMicroseconds(1);*/
-
-  // if there is a RW pin indicated, set it low to Write
-  /*if (_rw_pin != 255) { 
-    digitalWrite(_rw_pin, LOW);
-  }*/
-  
-  /*if (_displayfunction & LCD_8BITMODE) {
-    write8bits(value); 
-  } else {
-    write4bits(value>>4);
-    write4bits(value);
-  }*/
 
   write8bits(value);
 
@@ -171,12 +150,12 @@ void disp_write(char input);
 
 
 
-byte input_char;        // Input from bus
-bool valid_input;       // Whether or not reading value from bus
+byte input_char;        // Input from data_bus_in
+bool valid_input;       // Whether or not reading value from data_bus_in
 int cur_row;            // Current row
 int cur_col;            // Current column
 byte shadow[ROWS][COLS];  // Shadow of display
-byte disp_buffer[ROWS * COLS];  //  Display buffer
+byte disp_buffer[ROWS * COLS];  //  Display buffer 
 int buffer_index;               //  Buffer index
 
 // Buffer (queue)
@@ -188,7 +167,8 @@ byte queue_pos = 0;      // Position in queue
 byte disp_en = 2;
 byte disp_en_val;
 byte ascii_or_int = 10;  // If 0: Print as ASCII; if 1: Print as integer (hex)
-byte bus[] = {14, 15, 16, 17, 18, 19, 8, 9}; // A0, A1, A2, A3, A4, A5, D8, D9
+//byte data_bus_in[] = {14, 15, 16, 17, 18, 19, 8, 9}; // A0, A1, A2, A3, A4, A5, D8, D9
+byte data_bus_in[] = {14, 15, 16, 17, 18, 19, 19, 20}; // A0, A1, A2, A3, A4, A5, D8, D9
 
 // Edge detector vars
 bool was_low = true;    // For positive edge
@@ -285,6 +265,7 @@ void disp_write(char input)
      //lcd.setCursor(cur_col, cur_row);
     //lcd.write(input);
     write(input);
+   // Serial.write(input);
 
     // Write character to shadow
     shadow[cur_row][cur_col] = input;
@@ -297,17 +278,17 @@ void setup() {
   lcd.cursor();
   lcd.blink();
 
-  // Set Bus pins
+  // Set data_bus_in pins
   pinMode(disp_en, INPUT);      //  Set D13 as "display_enable" input
   pinMode(ascii_or_int, INPUT); //  Set D8 as "ascii or integer" input
   int i;
-  for (i = 0; i < sizeof(bus); i++)
+  for (i = 0; i < sizeof(data_bus_in); i++)
   {
-    pinMode(bus[i], INPUT); 
+    pinMode(data_bus_in[i], INPUT); 
   }
 
   for (i = 0; i < 7; i++)
-    pinMode(_data_pins[i], OUTPUT);
+    pinMode(display_data_out[i], OUTPUT);
 
   valid_input = false;
   cur_row = 0;
@@ -327,7 +308,7 @@ void setup() {
   //PCMSK0 |= B00100000;  // Activate interrupt on display enable (D13) pin (PCINT3)
 
   // Debug
- // Serial.begin(57600); // open the serial port at 57600 bps:
+// Serial.begin(57600); // open the serial port at 57600 bps:
 
   /*queue[0] = {'A', 0};
   queue[1] = {'B', 0};
@@ -354,33 +335,14 @@ int num_low_interrupts = 0;
 void disp_en_handler()
 {
   // Read input and add to queue
-  input_type = (PINB & 0x04) >> 2;
-  input_char = (PINC & 0x3f) | ((PINB & 0x03) << 6);
+  input_type = (PINB & 0x04) >> 2; // PB2
+  input_char = (PINC & 0x3f) | ((PINB & 0x03) << 6);  // PB1, PB0, PC5..PC0
+  //input_char =  (PINC & 0x7f) | ((PINB & 0x02) << 6);;  // PC7..PC0
   queue[queue_pos++] = {input_char, input_type};
   num_low_interrupts++;
 }
 
-/*ISR (PCINT0_vect)
-{
-  //Serial.println("Interrupt!");
-  //if (!digitalRead(disp_en))
-  if (!(PINB & 0x20))
-  {
-    //Serial.println("Interrupt handler (disp_en is low)");
-    // Read input and add to queue
-    input_char = (PINC & 0x3f) | ((PINB & 0x03) << 6);
-    //input_type = digitalRead(ascii_or_int);
-    input_type = (PINB & 0x04) >> 2; 
-    //if (input_type)
-    //  input_type = 'I';
-    //else
-    //  input_type = 'A';
 
-    queue[queue_pos++] = {input_char, input_type};
-    num_low_interrupts++;
-  }
-  num_interrupts++;
-}*/
 
 void loop() {
   if (queue_pos != queue_base)
@@ -414,82 +376,6 @@ void loop() {
       disp_write(low_digit);
     }
 
-    // Print shadow
-    //Serial.println("Shadow:");
-    //int i, j;
-    //for (i = 0; i < ROWS; i++)
-    //{
-    //  for (j = 0; j < COLS; j++)
-    //    Serial.print((char)shadow[i][j]);
-    //  Serial.println();
-    //}
-
-    //if (curr_char.input_char == 0x2E)
-    //{
-    //  lcd.print("\nlow_ints :");
-    //  lcd.print(num_low_interrupts);
-    //}
-
-    queue_base++;
+       queue_base++;
   }
 }
-
-/*
-void loop() {
-  // From bus
-  disp_en_val = digitalRead(disp_en);
-  if (was_high && disp_en_val == LOW)
-  {
-    valid_input = true;
-    was_high = false;
-  }
-  else if (disp_en_val == HIGH)
-  {
-    was_high = true;
-  }
-  //valid_input = true;
-  //int i;
-  //input_char = 0;
-  //for (i = 0; i < sizeof(bus); i++)
-  //{
-  //  input_char = input_char | (digitalRead(bus[i]) << i); 
-  //}
-  input_char = (PINC & 0x3f) | ((PINB & 0x03) << 6);
-
-  //input_char = PINB & 0x03;
-  //input_char = input_char << 6;
-  //input_char = input_char | (PINC & 0x3f);
-
-  // Display
-  if (valid_input)
-  {
-    
-    //Serial.print("Input: ");
-    //Serial.println(input_char, HEX);
-    //lcd.write(input_char);
-    if (!digitalRead(ascii_or_int)) // Print as ASCII
-    {
-      if (convert_input(&input_char) == PRINT_CHAR)
-      {
-        //Serial.print("printing ");
-        //Serial.print((char)input_char);
-        //Serial.println();
-        disp_write(input_char);
-      }
-      else
-      {
-        //Serial.println("printing escape character");
-      }
-    }
-    else  // Print as two 4-byte characters (print in hex)
-    {
-      byte high_digit = input_char >> 4;
-      byte low_digit = input_char & 0x0f;
-      convert_input(&high_digit);
-      convert_input(&low_digit);
-      disp_write(high_digit);
-      disp_write(low_digit);
-    }
-    valid_input = false;
-  }
-}*/
