768d43c54fb56daf1adb020f33a2e6c88ff1c944
Ron Keidar <ronkeidar45@gmail.com>
2026-02-12 23:15:29 -0800

    Shell with Command line buffer
    Shell collects chars up to the buffer limit and echo them to the display

--- Programs/Shell/shell.asm
+++ Programs/Shell/shell.asm
@@ -66,22 +66,70 @@ OUT _v, A
 OUT #1, A
 OUT DOT, A
 OUT #0, A
+OUT #3, A
+
 OUT NEW_LINE, A
 
 
 .define 2 kbd_addr	0xE207
+.define 2 command_line 0xDF00
+.define 1 max_command_length 10
 
-
+LDR R3, #0						// Empty command line
+SDW R3, %BP, #0					// Update Pointer value in memory
 
 :shell_loop
 
 LDA %DP, H, kbd_addr[1]
 LDA %DP, L, kbd_addr[0]
-
+LDA %BP, H, command_line[1]
+LDA %BP, L, command_line[0]
+// The command line is: 1 byte length, 10 bytes command 
 
 LDW R0, %DP, #0  
 TST R0, #0 // Was there any charecter?
+
 BEQ  :shell_loop
+LDR R1, #13 // 'Return'
+TST R0, R1 
+BEQ :dispatcher
 OUT R0, A
+// Add charecter to the command line
+LDW R3, %BP, #0 				// Current Command length
+LDR R1, max_command_length
+TST R3, R1 						// Has Command reached max_command_legth?    
+BEQ :shell_loop                 // Ignore the char after max length   
+INC R3							// Move pointer to next char
+SDW R3, %BP, #0					// Update Pointer value in memory
+MGA %BP, L, R3 					// Move BP to point next char
+SDW R0, %BP, #0					// store R0 as the next char
+JMP  :shell_loop
+
+:dispatcher
+OUT NEW_LINE, A
+// Test - Echo command
+OUT _C, A
+OUT _M, A
+OUT _D, A
+OUT COLON, A
+LDA %BP, L, command_line[0]		// Restore BP
+LDW R3, %BP, #0 				// Load Current Command length
+TST R3, #0
+BEQ :end_echo
+
+LDR R0, #1
+:echo_command
+MGA %BP, L, R0 	
+LDW R1, %BP, #0
+OUT R1, A
+INC R0
+TST R3, R0
+BPL :echo_command
+
+:end_echo
+OUT NEW_LINE, A
+LDA %BP, L, command_line[0]		// Restore BP
+LDR R3, #0
+SDW R3, %BP, #0					// Update Pointer value in memory
 JMP  :shell_loop
 
